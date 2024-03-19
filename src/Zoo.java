64b8da4f310261fa55508a092ae31034cc677038
prodii <pvalitiana@gmail.com>
2024-03-19 18:27:29 -0400

    CLASSE ZOO: CRÉATION DES VARIABLES (nom, pileGardiens, fileVisiteurs, nombreEnclos, lesENclos, nombreTOtalAnimaux)

--- src/Zoo.java
+++ src/Zoo.java
@@ -1,2 +1,10 @@
-public class Zoo {
+public class Zoo{
+    private String nom;
+    private Pile pileGardiens;
+    private File fileVisiteurs;
+    private int nombreEnclos;
+    private int[] lesEnclos = new int[nombreEnclos];
+    int nombreTotalAnimaux;
+
+
 }
