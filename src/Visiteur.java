ee8086d27f681cb3f24fa4be4ae7d14e33a90de1
prodii <pvalitiana@gmail.com>
2024-04-03 12:33:44 -0400

    CLASSE VISITEUR: Création du constructeur, getters, setters et méthodes demandées

---
+++ src/Visiteur.java
@@ -0,0 +1,59 @@
+public class Visiteur {
+    private String nom;
+    private int age;
+    private int nombreAnimaux;
+    private String[] especes;
+
+    public Visiteur(String nom, int age, String[] especes){
+        setNom(nom);
+        setAge(age);
+        setEspeces(especes);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //public int getNombreAnimaux() {
+      //  return nombreAnimaux;
+    //}
+
+    public void setNombreAnimaux(int nombreAnimaux) {
+        this.nombreAnimaux = nombreAnimaux;
+    }
+
+    public String[] getEspeces() {
+        return especes;
+    }
+
+    public void setEspeces(String[] especes) {
+        this.especes = especes;
+    }
+
+    public void entrerDansZoo(Zoo zoo){
+
+    }
+
+    public void entrerDansEnclo(Enclos enclo){
+
+    }
+    public void quitterZoo(){
+
+    }
+
+    public void quitterEnclo(Enclos enclo){
+
+    }
+}
