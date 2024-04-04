3ab913f0ddac777086e5647262ed79e51dca6ce2
prodii <pvalitiana@gmail.com>
2024-04-04 14:29:14 -0400

    CONSTRUCTEUTR ENCLOS + boilerplate

--- src/Captivite/Enclos.java
+++ src/Captivite/Enclos.java
@@ -5,8 +5,64 @@ import Employes.Veterinaire;
 
 public class Enclos {
     private String nom;
+    private int nombreAnimaux;
     private int capaciteMax;
+    private Animal[] animaux;
     private Veterinaire veterinaire;
     private Gardien gardien;
-    private Animal[] animaux;
+
+    public Enclos(String nom, int capaciteMax, Veterinaire veterinaire, Gardien gardien){
+        setNom(nom);
+        setCapaciteMax(capaciteMax);
+        setVeterinaire(veterinaire);
+        setGardien(gardien);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNombreAnimaux() {
+        return nombreAnimaux;
+    }
+
+    public void setNombreAnimaux(int nombreAnimaux) {
+        this.nombreAnimaux = nombreAnimaux;
+    }
+
+    public int getCapaciteMax() {
+        return capaciteMax;
+    }
+
+    public void setCapaciteMax(int capaciteMax) {
+        this.capaciteMax = capaciteMax;
+    }
+
+    public Animal[] getAnimaux() {
+        return animaux;
+    }
+
+    public void setAnimaux(Animal[] animaux) {
+        this.animaux = animaux;
+    }
+
+    public Veterinaire getVeterinaire() {
+        return veterinaire;
+    }
+
+    public void setVeterinaire(Veterinaire veterinaire) {
+        this.veterinaire = veterinaire;
+    }
+
+    public Gardien getGardien() {
+        return gardien;
+    }
+
+    public void setGardien(Gardien gardien) {
+        this.gardien = gardien;
+    }
 }
