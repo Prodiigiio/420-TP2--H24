6fd7290047c842ffc08f3ff5b76fa1e78a10d6f0
prodii <pvalitiana@gmail.com>
2024-04-04 15:31:40 -0400

    PILE & FILE : boilerPlate + constructeur

--- src/ca/cal/tp2/GestionDonnees/File.java
+++ src/ca/cal/tp2/GestionDonnees/File.java
@@ -1,4 +1,36 @@
 package ca.cal.tp2.GestionDonnees;
 
 public class File {
+    private Noeud premier;
+    private int nbElements;
+
+    public File(){
+        this.nbElements = 0;
+    }
+    public boolean estVide(){
+        return nbElements != 0;
+    }
+
+    public void ajouter(Noeud nouveauNoeud) {
+        if (estVide()) {
+            premier = nouveauNoeud;
+        } else {
+            getDernierNoeud().setSuivant(nouveauNoeud);
+        }
+    }
+
+    public Noeud pop(){
+        if (estVide()) return null;
+        Noeud courant = premier;
+        premier = premier.getSuivant();
+        return courant;
+    }
+
+    private Noeud getDernierNoeud() {
+        Noeud courant = premier;
+        while (courant.getSuivant() != null) {
+            courant = courant.getSuivant();
+        }
+        return courant;
+    }
 }
--- src/ca/cal/tp2/GestionDonnees/Pile.java
+++ src/ca/cal/tp2/GestionDonnees/Pile.java
@@ -1,7 +1,38 @@
 package ca.cal.tp2.GestionDonnees;
 
-public class Pile {
-    final int TAILLE_INITIALE = 4;
-    final int RATION_AGRANDISEMENT = 2;
+import ca.cal.tp2.Employes.Gardien;
 
+public class Pile {
+    private final int TAILLE_INITIALE = 4;
+    private final int RATION_AGRANDISEMENT = 2;
+
+    private Gardien[] gardiens;
+    private int nbElements;
+    private int capacite;
+
+    public Pile(){
+        setNbElements(0);
+        setCapacite(TAILLE_INITIALE);
+        this.gardiens = new Gardien[TAILLE_INITIALE];
+    }
+
+    public Gardien[] getGardiens() {
+        return gardiens;
+    }
+
+    public int getNbElements() {
+        return nbElements;
+    }
+
+    public void setNbElements(int nbElements) {
+        this.nbElements = nbElements;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
 }
