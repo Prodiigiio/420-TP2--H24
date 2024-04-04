37961b3e3a3012132c633d24ac9c9cd70475d528
prodii <pvalitiana@gmail.com>
2024-04-04 14:00:38 -0400

    CONSTRUCTEUR VETERINAIRE

--- src/Employes/Veterinaire.java
+++ src/Employes/Veterinaire.java
@@ -1,5 +1,29 @@
 package Employes;
 
+import Captivite.ClassificationAnimale;
+
 public class Veterinaire {
     private String nom;
+    private ClassificationAnimale specialiste;
+
+    public Veterinaire(String nom, ClassificationAnimale specialiste){
+        setNom(nom);
+        setSpecialiste(specialiste);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public ClassificationAnimale getSpecialiste() {
+        return specialiste;
+    }
+
+    public void setSpecialiste(ClassificationAnimale specialiste) {
+        this.specialiste = specialiste;
+    }
 }
