6ec9a0304ee8b76245268a1fb9d07af1d5474198
prodii <pvalitiana@gmail.com>
2024-04-04 14:10:40 -0400

    CONSTRUCTEUR GARDIEN + Gardien boilerPlate

--- src/Employes/Gardien.java
+++ src/Employes/Gardien.java
@@ -1,4 +1,42 @@
 package Employes;
 
 public class Gardien {
+    private String nom;
+    private int competence;
+    private static int idGenerale = 1000;
+    private int id;
+
+    public Gardien(String nom, int competence){
+        setNom(nom);
+        setCompetence(competence);
+        setId();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getCompetence() {
+        return competence;
+    }
+
+    public void setCompetence(int competence) {
+        this.competence = competence;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Chaque nouveau gardien obtien un ID unique
+     */
+    public void setId() {
+        idGenerale++;
+        this.id = idGenerale;
+    }
 }
