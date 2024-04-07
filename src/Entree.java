617d245f2a5502f0f23d3f4f44f7c755c6885a08
prodii <pvalitiana@gmail.com>
2024-04-06 21:06:49 -0400

    CLASSE ENTREE: BILLETERIE

--- src/Entree.java
+++ src/Entree.java
@@ -8,12 +8,16 @@ public class Entree {
     public Entree(Visiteur visiteur, Zoo zoo) {
         setVisiteur(visiteur);
         setZoo(zoo);
+        setNombreEnclos(nbEnclosAccessibles());
+        setPrix(billeterie());
     }
 
     public int getNombreEnclos() {
         return nombreEnclos;
     }
-
+    public void setNombreEnclos(int nombreEnclos) {
+        this.nombreEnclos = nombreEnclos;
+    }
 
     public Enclos[] getLesEnclos() {
         return lesEnclos;
@@ -23,7 +27,9 @@ public class Entree {
     public double getPrix() {
         return prix;
     }
-
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
 
     public Visiteur getVisiteur() {
         return visiteur;
@@ -61,5 +67,17 @@ public class Entree {
 
     private int nbEnclosAccessibles(){
         return getEnclosSecure().length;
+    }
+
+    private double billeterie(){
+        int nombreAnimaux = zoo.getNombreTotalAnimaux();
+        double prixInitial = getNombreEnclos() * 10;
+        double surchargeAnimal = nombreAnimaux;
+
+        if(visiteur.getAge() < 12){
+            return 0.0;
+        } else {
+            return (prixInitial + surchargeAnimal) * 0.5;
+        }
     }
 }
