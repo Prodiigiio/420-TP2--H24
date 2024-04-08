52c287621837ae797b769fea7cf884e0947f5a89
prodii <pvalitiana@gmail.com>
2024-04-08 17:52:13 -0400

    CLASSE ZOO: "Guard claws" pour enclosSansGardienAvecMoinsAnimaux() - MODIFIÉ #3

--- src/Zoo.java
+++ src/Zoo.java
@@ -101,7 +101,7 @@ public class Zoo {
     }
 
     private Enclos enclosSansGardienAvecMoinsAnimaux(Enclos[] enclos){
-        if(enclos[0].getNombreAnimaux() == 0) return null;
+        if(enclos[0] == null) return null;
         int nbAnimaux = enclos[0].getNombreAnimaux();
         Enclos enclosAvecLeMoinsAnimaux = enclos[0];
         for (Enclos e : enclos){
