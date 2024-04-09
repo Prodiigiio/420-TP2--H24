bf5944ef0dcbb18216545dcff0384174e0cb7b68
prodii <pvalitiana@gmail.com>
2024-04-08 22:21:32 -0400

    CLASSE Pile: Javadoc

--- src/Pile.java
+++ src/Pile.java
@@ -7,37 +7,59 @@ public class Pile {
     private int nbElements;
     private int capacite;
 
+    /**
+     * le constructeur de classe
+     */
     public Pile(){
         setNbElements(0);
         setCapacite(TAILLE_INITIALE);
         this.gardiens = new Gardien[TAILLE_INITIALE];
     }
 
+    /**
+     * Getter pour les gardiens
+     * @return les gardiens
+     */
     public Gardien[] getGardiens() {
         return gardiens;
     }
 
+    /**
+     * Getter pour le nombre d'éléments
+     * @return le nombre d'éléments
+     */
     public int getNbElements() {
         return nbElements;
     }
 
+    /**
+     * Setter pour le nombre d'éléments
+     * @param nbElements dont on veut changer
+     */
     public void setNbElements(int nbElements) {
         this.nbElements = nbElements;
     }
 
+    /**
+     * Getter pour la capacité
+     * @return la capacité
+     */
     public int getCapacite() {
         return capacite;
     }
 
+    /**
+     * Setter pour la capacité
+     * @param capacite dont on veut changer
+     */
     public void setCapacite(int capacite) {
         this.capacite = capacite;
     }
 
-    public void push(Gardien nouveauGardien){
-        adapterCapacite();
-        embaucherGardien(nouveauGardien);
-    }
-
+    /**
+     * Méthode pour enlever le gardien le plus récent
+     * @return le gardien le plus récent
+     */
     public Gardien pop(){
         if(getNbElements() == 0) return null;
         Gardien gardienRecent = gardiens[getNbElements() -1];
@@ -46,6 +68,9 @@ public class Pile {
         return gardienRecent;
     }
 
+    /**
+     * Méthode pour augmenter la capacité de la pile au cas où il n'y a plus de place
+     */
     private void adapterCapacite() {
         if(!(nbElements == capacite)) return;
         Gardien[] gardienAgrandi = new Gardien[getCapacite() * RATION_AGRANDISEMENT];
@@ -55,6 +80,10 @@ public class Pile {
         this.gardiens = gardienAgrandi;
     }
 
+    /**
+     * Méthode pour embaucher un nouveau gardien
+     * @param leNouveau gardien à embaucher
+     */
     private void embaucherGardien( Gardien leNouveau){
         for (int i = 0; i < getGardiens().length; i++) {
             if(getGardiens()[i] == null) {
@@ -65,6 +94,10 @@ public class Pile {
         }
     }
 
+    /**
+     * Méthode pour afficher la pile
+     * @return la pile affichée
+     */
     @Override
     public String toString() {
         String pile = gardiens.length + " gardiens" + "(capacité" + getNbElements() + "): ";
