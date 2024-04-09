21f86561e441cc315520f52c6a31e30096a16211
prodii <pvalitiana@gmail.com>
2024-04-08 22:02:11 -0400

    CLASSE Animal: javadoc + besoinAlimentaire()

--- src/Animal.java
+++ src/Animal.java
@@ -16,54 +16,114 @@ public class Animal {
         setEspece(espece);
     }
 
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getRegimeAlimentaire() {
-        return regimeAlimentaire;
-    }
-
-    public void setRegimeAlimentaire(String regimeAlimentaire) {
-        this.regimeAlimentaire = regimeAlimentaire;
-    }
-
-    public double getPoids() {
-        return poids;
-    }
-
-    public void setPoids(double poids) {
-        this.poids = poids;
-    }
-
-    public Classe getClassificationAnimale() {
-        return classe;
-    }
-
-    public void setClassificationAnimale(Classe classe) {
-        this.classe = classe;
-    }
-
+    /**
+     * getter pour l'espèce
+     * @return l'espèce
+     */
     public String getEspece() {
         return espece;
     }
 
-    public void setEspece(String espece) {
-        this.espece = espece;
+    /**
+     * getter pour le poids de l'animal
+     * @return le poids de l'animal
+     */
+    public double getPoids() {
+        return poids;
     }
 
+    /**
+     * getter pour le besoin alimentaire
+     * @return le besoin alimentaire
+     */
     public double getBesoinAlimentaire() {
         return besoinAlimentaire;
     }
 
-    public void setBesoinAlimentaire(double besoinAlimentaire) {
-        this.besoinAlimentaire = besoinAlimentaire;
+    public void besoinAlimentaire() {
+        switch (getClassificationAnimale()) {
+            case Poisson:
+                this.besoinAlimentaire = getPoids() * 0.01; // Piscivore
+                break;
+            case Cetace:
+            case Mammifere:
+                this.besoinAlimentaire = getPoids() * 0.05; // Carnivore
+                break;
+            case Reptile:
+                this.besoinAlimentaire = getPoids() * 0.03; // Herbivore
+                break;
+            case Oiseau:
+                this.besoinAlimentaire = getPoids() * 0.02; // Omnivore
+                break;
+            default:
+                this.besoinAlimentaire = 0;
+        }
     }
 
+    /**
+     * getter pour le nom
+     * @return le nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * setter pour le nom
+     * @param nom dont on veut changer
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * setter pour le régime alimentaire
+     * @param regimeAlimentaire dont on souhaite changer
+     */
+    public void setRegimeAlimentaire(String regimeAlimentaire) {
+        this.regimeAlimentaire = regimeAlimentaire;
+    }
+
+    /**
+     * setter pour le poids de l'animal
+     * @param poids dont on souhaite changer
+     */
+    public void setPoids(double poids) {
+        this.poids = poids;
+    }
+
+    /**
+     * getter pour la classification animale
+     * @return la classification animale
+     */
+    public Classe getClassificationAnimale() {
+        return classe;
+    }
+
+    /**
+     * setter pour la classification animale
+     * @param classe dont on souhaite changer
+     */
+    public void setClassificationAnimale(Classe classe) {
+        this.classe = classe;
+    }
+
+
+
+    /**
+     * setter pour l'espèce
+     * @param espece dont on souhaite changer
+     */
+    public void setEspece(String espece) {
+        this.espece = espece;
+    }
+
+
+
+    /**
+     * Affichage des informations de l'animal
+     * @return les informations de l'animal
+     */
     @Override
     public String toString() {
             return ("--- Le " + getClassificationAnimale() + " nommé " + getNom() + " pèse " + getPoids() + "kg et son besoin alimentaire " + getBesoinAlimentaire() + " de kg \n");
