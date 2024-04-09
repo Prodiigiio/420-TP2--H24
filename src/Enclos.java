c370d5220859ed64b5de3eb623a7668d7379274c
prodii <pvalitiana@gmail.com>
2024-04-08 21:41:36 -0400

    CLASSE Enclos: javadoc + mise en ordre des méthodes en se basant sur le document TP2

--- src/Enclos.java
+++ src/Enclos.java
@@ -6,6 +6,13 @@ public class Enclos {
     private Veterinaire veterinaire;
     private Gardien gardien;
 
+    /**
+     * le constructeur de classe
+     * @param nom pour initialiser le nom
+     * @param capaciteMax pour initialiser la capacité maximale
+     * @param veterinaire pour initialiser le veterinaire
+     * @param gardien pour initialiser le gardien
+     */
     public Enclos(String nom, int capaciteMax, Veterinaire veterinaire, Gardien gardien){
         setNom(nom);
         setCapaciteMax(capaciteMax);
@@ -13,59 +20,110 @@ public class Enclos {
         setGardien(gardien);
     }
 
+    /**
+     * Getter pour le nom
+     * @return le nom
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Setter pour le nom
+     * @param nom dont on veut changer
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
-    public int getNombreAnimaux() {
-        return nombreAnimaux;
-    }
 
-    public void setNombreAnimaux(int nombreAnimaux) {
-        this.nombreAnimaux = nombreAnimaux;
-    }
 
+    /**
+     * Getter pour le capacitée maximale
+     * @return la capacité maximale
+     */
     public int getCapaciteMax() {
         return capaciteMax;
     }
 
+/**
+     * Setter pour la capacité maximale
+     * @param capaciteMax dont on veut changer
+     */
     public void setCapaciteMax(int capaciteMax) {
         this.capaciteMax = capaciteMax;
     }
 
-    public Animal[] getAnimaux() {
-        return animaux;
-    }
-
-    public void setAnimaux(Animal[] animaux) {
-        this.animaux = animaux;
-    }
-
-    public Veterinaire getVeterinaire() {
-        return veterinaire;
-    }
-
-    public void setVeterinaire(Veterinaire veterinaire) {
-        this.veterinaire = veterinaire;
-    }
-
+    /**
+     * Getter pour le gardien
+     * @return le gardien ou null au cas où il n'y a pas de gardien
+     */
     public Gardien getGardien() {
+        if(gardien == null) return null;
         return gardien;
     }
 
+    /**
+     * Setter pour le gardien
+     * @param gardien dont on veut changer
+     */
     public void setGardien(Gardien gardien) {
         this.gardien = gardien;
     }
 
+    /**
+     * Getter pour le vétérinaire
+     * @return le vétérinaire
+     */
+    public Veterinaire getVeterinaire() {
+        return veterinaire;
+    }
+
+    /**
+     * Setter pour le vétérinaire
+     * @param veterinaire dont on veut changer
+     */
+    public void setVeterinaire(Veterinaire veterinaire) {
+        this.veterinaire = veterinaire;
+    }
+
+    /**
+     * Ajouter un animal dans l'enclos
+     * @param lesAnimaux dont on veut ajouter (tableau d'animaux)
+     */
     public void ajouterAnimaux(Animal[] lesAnimaux){
         if (lesAnimaux.length -1 > getCapaciteMax()) return;
         setAnimaux(lesAnimaux);
     }
 
+    /**
+     * Getter pour le nombre d'animaux
+     * @return le nombre d'animaux
+     */
+    public int getNombreAnimaux() {
+        return nombreAnimaux;
+    }
+
+    /**
+     * Getter pour les animaux
+     * @return les animaux
+     */
+    public Animal[] getAnimaux() {
+        return animaux;
+    }
+
+    /**
+     * Setter pour les animaux
+     * @param animaux dont on veut changer
+     */
+    public void setAnimaux(Animal[] animaux) {
+        this.animaux = animaux;
+    }
+
+    /**
+     * Méthode pour afficher les informations de l'enclos
+     * @return les informations de l'enclos
+     */
     @Override
     public String toString() {
         String resultat = "Enclos " + getNom() + " est peuplé avec " + getNombreAnimaux() + " animaux. Sa capacité est de " + getCapaciteMax() + " animaux. "
