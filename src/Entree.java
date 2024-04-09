1a7159343549d8586cbbc14bc910afcf9a1d6c90
prodii <pvalitiana@gmail.com>
2024-04-08 22:06:48 -0400

    CLASSE Entree: javadoc

--- src/Entree.java
+++ src/Entree.java
@@ -5,6 +5,11 @@ public class Entree {
     private Visiteur visiteur;
     private Zoo zoo;
 
+    /**
+     * le constructeur de classe
+     * @param visiteur pour initialiser le visiteur
+     * @param zoo pour initialiser le zoo
+     */
     public Entree(Visiteur visiteur, Zoo zoo) {
         setVisiteur(visiteur);
         setZoo(zoo);
@@ -13,39 +18,74 @@ public class Entree {
     }
 
 
+    /**
+     * getter pour le nombre d'enclos
+     * @return le nombre d'enclos
+     */
     public int getNombreEnclos() {
         return nombreEnclos;
     }
+
+    /**
+     * setter pour le nombre d'enclos
+     * @param nombreEnclos dont on veut changer
+     */
     public void setNombreEnclos(int nombreEnclos) {
         this.nombreEnclos = nombreEnclos;
     }
 
+    /**
+     * getter pour les enclos
+     * @return les enclos
+     */
     public Enclos[] getEnclos() {
         return lesEnclos;
     }
 
+    /**
+     * Getter pour le prix
+     * @return le prix
+     */
     public double getPrix() {
         return prix;
     }
+
+    /**
+     * Setter pour le prix
+     * @param prix dont on veut changer
+     */
     public void setPrix(double prix) {
         this.prix = prix;
     }
 
-    public Visiteur getVisiteur() {
-        return visiteur;
-    }
+    /**
+     * Setter pour les enclos
+     * @param visiteur
+     */
     public void setVisiteur(Visiteur visiteur) {
         this.visiteur = visiteur;
     }
 
+    /**
+     * Getter pour le zoo
+     * @return le zoo
+     */
     public Zoo getZoo() {
         return zoo;
     }
 
+    /**
+     * Setter pour le zoo
+     * @param zoo dont on veut changer
+     */
     public void setZoo(Zoo zoo) {
         this.zoo = zoo;
     }
 
+    /**
+     * Getter pour les enclos contenant au moins un gardien
+     * @return un enclos sécurisé, donc accessible
+     */
     private Enclos[] getEnclosSecure(){
 
         int enclosAvecGardien = 0;
@@ -65,10 +105,18 @@ public class Entree {
         return enclosAccessibles;
     }
 
+    /**
+     * Méthode pour compter le nombre d'enclos accessibles
+     * @return le nombre d'enclos accessibles
+     */
     private int nbEnclosAccessibles(){
         return getEnclosSecure().length;
     }
 
+    /**
+     * Méthode pour calculer le prix du billet à la billeterie
+     * @return le prix de la billeterie
+     */
     private double billeterie(){
         int nombreAnimaux = zoo.getNombreTotalAnimaux();
         double prixInitial = getNombreEnclos() * 10;
@@ -81,6 +129,9 @@ public class Entree {
         }
     }
 
+    /**
+     * Méthode pour afficher les enclos accessibles
+     */
     public void afficherEnclos(){
         System.out.println("Les enclos accessibles pour " + visiteur.getNom() + " sont : ");
         Enclos[] enclos = getEnclos();
